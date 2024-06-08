ee6a4e7fb67118e1935fcc25cb835fee8dbc23df
AdmiralXy <gameproblem80@gmail.com>
2024-06-09 00:34:25 +0300

    fix checkstyle

--- commons-cqrs/src/main/java/com/admiralxy/commons/cqrs/mediator/Mediator.java
+++ commons-cqrs/src/main/java/com/admiralxy/commons/cqrs/mediator/Mediator.java
@@ -12,7 +12,6 @@ public class Mediator {
 
     private static final String HANDLER_POSTFIX = "Handler";
 
-
     private final ApplicationContext context;
 
     public <R, T extends Request<R>> R send(T request) {
@@ -22,12 +21,7 @@ public class Mediator {
         if (handlerBean instanceof RequestHandler<?, ?>) {
             @SuppressWarnings("unchecked")
             var handler = (RequestHandler<T, R>) handlerBean;
-
-
-
-
-            return handler.handle(request); //asdasdas
-            //asdasdas
+            return handler.handle(request);
         }
 
         throw new IllegalStateException("Handler not found for " + request.getClass().getName());
