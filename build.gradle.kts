65c093fe33fc16ce0e469755a21e406f5a7b61d8
AdmiralXy <gameproblem80@gmail.com>
2024-06-09 18:06:05 +0300

    add maven publish job

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
 	java
 	checkstyle
 	idea
+	`java-library`
 	`maven-publish`
+	signing
 	id("com.github.ben-manes.versions") version libs.versions.ben.manes.versions.get()
 }
 
@@ -20,6 +22,7 @@ subprojects {
 	apply(plugin = "java")
 	apply(plugin = "checkstyle")
 	apply(plugin = "maven-publish")
+	apply(plugin = "signing")
 
 	repositories {
 		mavenCentral()
@@ -43,7 +46,47 @@ subprojects {
 		publications {
 			create<MavenPublication>("mavenJava") {
 				from(components["java"])
+
+				pom {
+					name = "Spring AdmiralXy Commons"
+					description = "Utility library for Spring Boot applications"
+					url = "https://github.com/AdmiralXy/spring-admiralxy-commons"
+
+					licenses {
+						license {
+							name = "The Apache License, Version 2.0"
+							url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+						}
+					}
+					developers {
+						developer {
+							id = "admiralxy"
+							name = "AdmiralXy"
+							email = "gameproblem80@gmail.com"
+						}
+					}
+					scm {
+						connection = "scm:git:git://github.com/AdmiralXy/spring-admiralxy-commons.git"
+						developerConnection = "scm:git:ssh://github.com/AdmiralXy/spring-admiralxy-commons.git"
+						url = "https://github.com/AdmiralXy/spring-admiralxy-commons"
+					}
+				}
 			}
 		}
+		repositories {
+			maven {
+				name = "sonatype"
+				url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+				credentials {
+					username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+					password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+				}
+			}
+		}
+	}
+
+	signing {
+		useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PASSPHRASE"))
+		sign(publishing.publications["mavenJava"])
 	}
 }
